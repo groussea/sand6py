46324a0f603ed738f3ebb3f96e2905cb4707d7b3
Gilles Daviet <gilles.daviet@inria.fr>
2016-01-16 19:19:50 +0100

    CMake scripts look into vendor/ directory

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -17,13 +17,13 @@
 
 if(NOT Eigen3_FIND_VERSION)
   if(NOT Eigen3_FIND_VERSION_MAJOR)
-    set(Eigen3_FIND_VERSION_MAJOR 2)
+	set(Eigen3_FIND_VERSION_MAJOR 2)
   endif(NOT Eigen3_FIND_VERSION_MAJOR)
   if(NOT Eigen3_FIND_VERSION_MINOR)
-    set(Eigen3_FIND_VERSION_MINOR 91)
+	set(Eigen3_FIND_VERSION_MINOR 91)
   endif(NOT Eigen3_FIND_VERSION_MINOR)
   if(NOT Eigen3_FIND_VERSION_PATCH)
-    set(Eigen3_FIND_VERSION_PATCH 0)
+	set(Eigen3_FIND_VERSION_PATCH 0)
   endif(NOT Eigen3_FIND_VERSION_PATCH)
 
   set(Eigen3_FIND_VERSION "${Eigen3_FIND_VERSION_MAJOR}.${Eigen3_FIND_VERSION_MINOR}.${Eigen3_FIND_VERSION_PATCH}")
@@ -41,15 +41,15 @@ macro(_eigen3_check_version)
 
   set(EIGEN3_VERSION ${EIGEN3_WORLD_VERSION}.${EIGEN3_MAJOR_VERSION}.${EIGEN3_MINOR_VERSION})
   if(${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
-    set(EIGEN3_VERSION_OK FALSE)
+	set(EIGEN3_VERSION_OK FALSE)
   else(${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
-    set(EIGEN3_VERSION_OK TRUE)
+	set(EIGEN3_VERSION_OK TRUE)
   endif(${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
 
   if(NOT EIGEN3_VERSION_OK)
 
-    message(STATUS "Eigen3 version ${EIGEN3_VERSION} found in ${EIGEN3_INCLUDE_DIR}, "
-                   "but at least version ${Eigen3_FIND_VERSION} is required")
+	message(STATUS "Eigen3 version ${EIGEN3_VERSION} found in ${EIGEN3_INCLUDE_DIR}, "
+				   "but at least version ${Eigen3_FIND_VERSION} is required")
   endif(NOT EIGEN3_VERSION_OK)
 endmacro(_eigen3_check_version)
 
@@ -62,16 +62,17 @@ if (EIGEN3_INCLUDE_DIR)
 else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
-      PATHS
-      $ENV{EIGEN_ROOT}
-      ${BOGUS_ROOT}/External
-      ${CMAKE_INSTALL_PREFIX}/include
-      ${KDE4_INCLUDE_DIR}
-      PATH_SUFFIXES eigen3 eigen
-    )
+	  HINTS
+	  ${CMAKE_CURRENT_SOURCE_DIR}/vendor
+	  PATHS
+	  $ENV{EIGEN_ROOT}
+	  ${CMAKE_INSTALL_PREFIX}/include
+	  ${KDE4_INCLUDE_DIR}
+	  PATH_SUFFIXES eigen3 eigen
+	)
 
   if(EIGEN3_INCLUDE_DIR)
-    _eigen3_check_version()
+	_eigen3_check_version()
   endif(EIGEN3_INCLUDE_DIR)
 
   include(FindPackageHandleStandardArgs)
