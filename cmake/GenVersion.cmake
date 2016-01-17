2412a341207aa890b9962477a0b1c1aebd764118
Gilles Daviet <gilles.daviet@inria.fr>
2016-01-17 11:52:31 +0100

    Default version string if not in git repo

--- cmake/GenVersion.cmake
+++ cmake/GenVersion.cmake
@@ -1,21 +1,34 @@
 
 MESSAGE( STATUS "Updating git revision" )
 
+SET( GIT_CMD    git )
+SET( D6_VERSION "1.0-review" )
+
 # Get the current working branch
 execute_process(
-  COMMAND git rev-parse --abbrev-ref HEAD
+  COMMAND ${GIT_CMD} rev-parse --abbrev-ref HEAD
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  RESULT_VARIABLE GIT_OK
   OUTPUT_VARIABLE GIT_BRANCH
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
+if( NOT( ${GIT_OK} EQUAL 0 ) )
+	SET( GIT_BRANCH "release" )
+endif()
+
 # Get the latest abbreviated commit hash of the working branch
 execute_process(
-  COMMAND git log -1 --format=%h
+  COMMAND ${GIT_CMD} log -1 --format=%h
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  RESULT_VARIABLE GIT_OK
   OUTPUT_VARIABLE GIT_COMMIT_HASH
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+
+if( NOT( ${GIT_OK} EQUAL 0 ) )
+	SET( GIT_COMMIT_HASH ${D6_VERSION} )
+endif()
 
 string(TIMESTAMP COMPILE_TIME "%Y-%m-%d %H:%M")
 
