5e063fb45e0ca6819297bf84481710a66868f5f5
Gilles Daviet <gilles.daviet@inria.fr>
2016-01-25 16:47:43 +0100

    Started multidim infra

--- src/utils/alg.hh
+++ src/utils/alg.hh
@@ -3,17 +3,37 @@
 
 #include "scalar.hh"
 
+
 #include <Eigen/Core>
 
 namespace d6 {
+
+#ifdef DIM2
+static constexpr Index WD = 2 ;
+static constexpr Index SD = 3 ;
+#else
+static constexpr Index WD = 3 ;
+static constexpr Index SD = 6 ;
+#endif
 
 typedef Eigen::Matrix< Scalar, Eigen::Dynamic, 1 > DynVec;
 typedef Eigen::Array < Scalar, Eigen::Dynamic, 1 > DynArr;
 typedef Eigen::Matrix< Scalar, Eigen::Dynamic, Eigen::Dynamic > DynMat;
 
-typedef Eigen::Matrix< Scalar, 3, 3 > Mat ;
-typedef Eigen::Matrix< Scalar, 3, 1 > Vec ;
-typedef Eigen::Array < Scalar, 3, 1 > Arr ;
+typedef Eigen::Matrix< Scalar, WD, WD > Mat ;
+typedef Eigen::Matrix< Scalar, WD,  1 > Vec ;
+typedef Eigen::Array < Scalar, WD,  1 > Arr ;
+
+typedef Eigen::Matrix< Scalar, SD, SD > MatS ;
+typedef Eigen::Matrix< Scalar, SD,  1 > VecS ;
+typedef Eigen::Array < Scalar, SD,  1 > ArrS ;
+
+typedef Eigen::Matrix< Index, WD, 1 > VecWi ;
+typedef Eigen::Array < Index, WD, 1 > ArrWi ;
+
+typedef Eigen::Matrix< Scalar, 3, 3 > Mat33 ;
+typedef Eigen::Matrix< Scalar, 3, 1 > Vec3 ;
+typedef Eigen::Array < Scalar, 3, 1 > Arr3 ;
 
 typedef Eigen::Matrix< Index, 3, 1 > Vec3i ;
 typedef Eigen::Array < Index, 3, 1 > Arr3i ;
@@ -21,8 +41,11 @@ typedef Eigen::Array < Index, 3, 1 > Arr3i ;
 typedef Eigen::Matrix< Scalar, 6, 6 > Mat66 ;
 typedef Eigen::Matrix< Scalar, 6, 1 > Vec6 ;
 
+typedef Eigen::Matrix< Scalar, WD, Eigen::Dynamic > DynMatW ;
+typedef Eigen::Matrix< Scalar, SD, Eigen::Dynamic > DynMatS ;
 typedef Eigen::Matrix< Scalar, 3, Eigen::Dynamic > DynMat3 ;
 typedef Eigen::Matrix< Scalar, 6, Eigen::Dynamic > DynMat6 ;
+
 
 template < Index Dimension >
 struct Segmenter {
